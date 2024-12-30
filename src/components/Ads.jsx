d146c68167d6bcce98c4f7945797e487096e343e
nm21205 <nm21205@naver.com>
2024-12-30 17:14:27 +0900

    241231

--- src/components/Ads.jsx
+++ src/components/Ads.jsx
@@ -1,38 +1,18 @@
-import React, { useEffect } from 'react';
-import { Helmet } from 'react-helmet';
+import React from 'react';
+import GoogleAds from 'react-adsense';
 
 const Ads = () => {
-    useEffect(() => {
-        const handleAds = () => {
-            if (window.adsbygoogle) {
-                window.adsbygoogle.push({});
-            }
-        };
-
-        // 스크립트가 로드된 후에 handleAds를 호출합니다.
-        if (window.adsbygoogle) {
-            handleAds();
-        } else {
-            window.addEventListener('load', handleAds);
-        }
-
-        return () => {
-            window.removeEventListener('load', handleAds);
-        };
-    }, []);
-
     return (
         <div>
-            <Helmet>
-                <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9677892204769800" crossOrigin="anonymous"></script>
-                <meta name="google-adsense-account" content="ca-pub-9677892204769800" />
-            </Helmet>
-            <ins className="adsbygoogle"
-                style={{ display: 'block' }}
-                data-ad-client="ca-pub-9677892204769800"
-                data-ad-slot="4693054575" // 여기에 실제 광고 슬롯 ID를 입력하세요.
-                data-ad-format="auto"></ins>
-        </div>
+
+        <GoogleAds
+          client='ca-pub-9677892204769800' // 여기에 실제 퍼블리셔 ID를 입력하세요.
+          slot='4693054575' // 여기에 실제 광고 슬롯 ID를 입력하세요.
+          style={{ display: 'block' }}
+          format='auto'
+          responsive='true'
+        />
+      </div>
     );
 };
 
